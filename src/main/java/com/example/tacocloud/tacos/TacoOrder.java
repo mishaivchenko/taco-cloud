03f277c88c57526c7f4530bf00d99e595f7929c7
mykhailivchenko <mivchenko@exadel.com>
2022-11-01 22:19:25 +0200

    [SIA-1] Spring Boot + MVC. Model + View have been implemented.

---
+++ src/main/java/com/example/tacocloud/tacos/TacoOrder.java
@@ -0,0 +1,37 @@
+package com.example.tacocloud.tacos;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryName;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryStreet;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryCity;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryState;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryZip;
+    @CreditCardNumber
+    private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
+    private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+
+}
